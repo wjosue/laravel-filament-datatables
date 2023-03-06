5768b08b0357bcf223661ddd400039ea8827b429
Wagner <wjosue@gmail.com>
2023-03-06 20:07:54 -0300

    Criação de models, seus relacionamentos e resource section

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -39,4 +39,14 @@ class Student extends Model
     protected $fillable = [
       'section_id', 'class_id', 'name', 'email', 'address', 'phone_number'
     ];
+
+    public function class()
+    {
+        return $this->belongsTo(Classes::class, 'class_id');
+    }
+
+    public function section()
+    {
+        return $this->belongsTo(Section::class);
+    }
 }
